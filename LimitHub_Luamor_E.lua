d8f9da9ff55fab69b81314a996632fc6b0b5d746
KeplerRBLX <keplerpw1@gmail.com>
2025-07-18 08:51:20 +0800

    Update LimitHub_Luamor_E.lua

--- LimitHub_Luamor_E.lua
+++ LimitHub_Luamor_E.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/Yw0XOZUDFC/0", true))()
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/FakeModz/LimitHub/refs/heads/main/LimitHub_Luarmor_E.lua')))()
+loadstring(game:HttpGet("https://pastefy.app/6IyBJ4xa/raw"))()
